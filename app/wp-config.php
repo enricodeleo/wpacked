32321e1899d8a88e6aab36fdfc0e37cb53f46b49
Enrico Deleo <enrico@lysergicstudio.com>
2015-05-30 15:03:27 +0200

    all sensitive configs to environment

--- app/wp-config.php
+++ app/wp-config.php
@@ -1,19 +1,21 @@
 <?php
 
-// Load all composer dependencies
+// Autoload Composer dependencies
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 
 // Load env variables
 Dotenv::load(dirname(__DIR__));
 Dotenv::required('ENVIRONMENT', array('development', 'staging', 'production'));
 
-// which environment is it?
-define('ENVIRONMENT', getenv('ENVIRONMENT') );
-
 $root_dir = dirname(__DIR__);
 
 $webroot_dir = $root_dir . '/app';
 define('WEBROOT_DIR', $webroot_dir);
+
+/**
+ * ENVIRONMENT
+ */
+define('WP_ENV', getenv('WP_ENV')); // development | staging | production
 
 /**
  * URLs
@@ -31,12 +33,13 @@ define('WP_CONTENT_URL', WP_HOME . CONTENT_DIR);
 /**
  * DB settings
  */
+
 $table_prefix = 'wp_';
 define('USE_MYSQL', getenv('USE_MYSQL'));  // turn it to true if you want to use MySQL instead of SQLite
-define('DB_NAME', getenv('DB_NAME'));
-define('DB_USER', getenv('DB_USER'));
-define('DB_PASSWORD', getenv('DB_PASSWORD'));
-define('DB_HOST', getenv('DB_HOST'));
+define('DB_NAME', 'dbname');
+define('DB_USER', 'dbuser');
+define('DB_PASSWORD', 'dbpassword');
+define('DB_HOST', 'localhost');
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 
@@ -49,37 +52,41 @@ define('DB_FILE', 'db.sqlite');
 /**
  * Authentication Unique Keys and Salts
  */
-define('AUTH_KEY',         getenv('AUTH_KEY'));
-define('SECURE_AUTH_KEY',  getenv('SECURE_AUTH_KEY'));
-define('LOGGED_IN_KEY',    getenv('LOGGED_IN_KEY'));
-define('NONCE_KEY',        getenv('NONCE_KEY'));
-define('AUTH_SALT',        getenv('AUTH_SALT'));
-define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
-define('LOGGED_IN_SALT',   getenv('LOGGED_IN_SALT'));
-define('NONCE_SALT',       getenv('NONCE_SALT'));
+define('AUTH_KEY',         't(;_#gm0;-C{$a`I$K)>v*%Pz-80U-ZN0?dfuNc)W0>4KvJ&.1((~ku-6.XUXj0_');
+define('SECURE_AUTH_KEY',  'd=e_HInEOsloj+JMz*OkNAP~JTX4(v&=tk;Q /2Ol+s#+C,L#mLz7gyR0$I:bE@d');
+define('LOGGED_IN_KEY',    '9J{!b!reZx+L2SWjKR7. X3JK66jG{-nf^|AA$xp~DD69K<B/)t;]Ng=37P5s-GC');
+define('NONCE_KEY',        '0O8[1z]/[G5PIQ6S/F!8F@j)al9[y%F}v_m~[/6X(pH4g7%cmTcH)qJVA~|.zOrK');
+define('AUTH_SALT',        'm~|^<ab)VdD9IMCZa0Z? ]pnQjn]k6[HUR!FmE8d8RGuDRb(KQCY<<cLQWDUYp<&');
+define('SECURE_AUTH_SALT', 'fKu~[oqY[20xAR|;S^*1x*?Y0>h~;`CNenxd7^E!@<jnnpRf%zD%-l(8EVvcrGs{');
+define('LOGGED_IN_SALT',   'i]O`J02T|B<?|;p1yecUZWD2sdGYur6f=&&MI8IYLHBQs~MUTpP-,+9jMp5ZJ!kx');
+define('NONCE_SALT',       '3S{O_/tYOPGB^s-},?$I<{5[A:%N75f!-h*lx.emj|B$U8h3)baV8i/0fX<oc=|d');
 
 /**
  * Custom Settings
  */
 define('AUTOMATIC_UPDATER_DISABLED', true);
-define('DISABLE_WP_CRON', getenv('DISABLE_WP_CRON')); 
-define('DISALLOW_FILE_EDIT', true); // don't add plugins or edit files within the dashboard 
+define('DISABLE_WP_CRON', false);
+define('DISALLOW_FILE_EDIT', true);
 
 /**
- * Increase default memory limit
+ * Increase memory
  */
 define( 'WP_MEMORY_LIMIT', '128M' );
 define( 'WP_MAX_MEMORY_LIMIT', '256M' );
 
 // Optional debug config
-if ( ENVIRONOMENT == 'development' ) {
-  @ini_set('error_reporting', E_ALL);
-  @ini_set('display_errors', '1');
-  @ini_set('log_errors', '1');
-  define( 'WP_DEBUG', true );
-  define( 'WP_DEBUG_DISPLAY', true );
-  define( 'WP_DEBUG_LOG', true );
-}
+@ini_set('error_reporting', E_ALL);
+@ini_set('display_errors', '1');
+@ini_set('log_errors', '1');
+define( 'WP_DEBUG', true );
+define( 'WP_DEBUG_DISPLAY', true );
+define( 'WP_DEBUG_LOG', true );
+
+
+// define( 'SCRIPT_DEBUG', true );
+// define( 'CONCATENATE_SCRIPTS', false );
+// define( 'AUTOMATIC_UPDATER_DISABLED', true );
+// define( 'WP_AUTO_UPDATE_CORE', false );
 
 /**
  * Bootstrap WordPress
